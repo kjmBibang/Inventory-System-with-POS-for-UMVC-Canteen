10bed5e14defef62640a402cbe34dc400cc3de48
Gabriel <r.mabanag.145839.tc@umindanao.edu.ph>
2025-11-27 22:42:57 +0800

    Updated User Class with Attributes and Multiple Methods

--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Inventory_System_with_POS_for_UMVC_Canteen.Models
 {
-    internal class User
+    abstract class User
     {
+        protected string userID { get; set; }
+        protected string username { get; set; } 
+        protected string passwordHash { get; set; }
+        protected int roleID { get; set; }
+
+        public User(string userID, string username, string passwordHash, int roleID)
+        {
+           
+        }
+        public string GetUsername()
+        {
+            return username;
+        }
+        public string GetUserID()
+        {
+            return userID;
+        }
+
     }
 }
