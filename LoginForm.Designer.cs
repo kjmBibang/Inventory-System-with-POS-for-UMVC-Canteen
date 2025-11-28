1c5490e639b7d701b15330cd402346ce02485601
Bryce San Jose <a.sanjose.145835.tc@umindanao.edu.ph>
2025-11-28 07:47:11 -0500

    Made LoginForm

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Inventory_System_with_POS_for_UMVC_Canteen
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.panelColor = new System.Windows.Forms.Panel();
+            this.txtDeveloperName = new System.Windows.Forms.TextBox();
+            this.txtWelcome = new System.Windows.Forms.TextBox();
+            this.picboxUMLOGO = new System.Windows.Forms.PictureBox();
+            this.picboxUSER = new System.Windows.Forms.PictureBox();
+            this.txtblankUsername = new System.Windows.Forms.TextBox();
+            this.txtblankPassword = new System.Windows.Forms.TextBox();
+            this.picboxPASS = new System.Windows.Forms.PictureBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panelColor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxUMLOGO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxUSER)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPASS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelColor
+            // 
+            this.panelColor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panelColor.BackColor = System.Drawing.Color.Maroon;
+            this.panelColor.Controls.Add(this.txtDeveloperName);
+            this.panelColor.Controls.Add(this.txtWelcome);
+            this.panelColor.Controls.Add(this.picboxUMLOGO);
+            this.panelColor.Location = new System.Drawing.Point(-1, 0);
+            this.panelColor.Name = "panelColor";
+            this.panelColor.Size = new System.Drawing.Size(303, 370);
+            this.panelColor.TabIndex = 0;
+            // 
+            // txtDeveloperName
+            // 
+            this.txtDeveloperName.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.txtDeveloperName.BackColor = System.Drawing.Color.Maroon;
+            this.txtDeveloperName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDeveloperName.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDeveloperName.ForeColor = System.Drawing.Color.White;
+            this.txtDeveloperName.Location = new System.Drawing.Point(173, 299);
+            this.txtDeveloperName.Multiline = true;
+            this.txtDeveloperName.Name = "txtDeveloperName";
+            this.txtDeveloperName.Size = new System.Drawing.Size(116, 55);
+            this.txtDeveloperName.TabIndex = 3;
+            this.txtDeveloperName.Text = "Developed by: Monkey Development";
+            this.txtDeveloperName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtWelcome
+            // 
+            this.txtWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtWelcome.BackColor = System.Drawing.Color.Maroon;
+            this.txtWelcome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtWelcome.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWelcome.ForeColor = System.Drawing.Color.White;
+            this.txtWelcome.Location = new System.Drawing.Point(3, 153);
+            this.txtWelcome.Multiline = true;
+            this.txtWelcome.Name = "txtWelcome";
+            this.txtWelcome.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtWelcome.Size = new System.Drawing.Size(300, 129);
+            this.txtWelcome.TabIndex = 2;
+            this.txtWelcome.Text = "Welcome to the Inventory System UMVC Canteen";
+            this.txtWelcome.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // picboxUMLOGO
+            // 
+            this.picboxUMLOGO.Image = ((System.Drawing.Image)(resources.GetObject("picboxUMLOGO.Image")));
+            this.picboxUMLOGO.Location = new System.Drawing.Point(85, 30);
+            this.picboxUMLOGO.Name = "picboxUMLOGO";
+            this.picboxUMLOGO.Size = new System.Drawing.Size(138, 117);
+            this.picboxUMLOGO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picboxUMLOGO.TabIndex = 1;
+            this.picboxUMLOGO.TabStop = false;
+            // 
+            // picboxUSER
+            // 
+            this.picboxUSER.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.picboxUSER.Image = ((System.Drawing.Image)(resources.GetObject("picboxUSER.Image")));
+            this.picboxUSER.Location = new System.Drawing.Point(308, 165);
+            this.picboxUSER.Name = "picboxUSER";
+            this.picboxUSER.Size = new System.Drawing.Size(25, 21);
+            this.picboxUSER.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picboxUSER.TabIndex = 4;
+            this.picboxUSER.TabStop = false;
+            // 
+            // txtblankUsername
+            // 
+            this.txtblankUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtblankUsername.Location = new System.Drawing.Point(339, 166);
+            this.txtblankUsername.Name = "txtblankUsername";
+            this.txtblankUsername.Size = new System.Drawing.Size(249, 20);
+            this.txtblankUsername.TabIndex = 6;
+            // 
+            // txtblankPassword
+            // 
+            this.txtblankPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtblankPassword.Location = new System.Drawing.Point(339, 216);
+            this.txtblankPassword.Name = "txtblankPassword";
+            this.txtblankPassword.Size = new System.Drawing.Size(249, 20);
+            this.txtblankPassword.TabIndex = 7;
+            this.txtblankPassword.UseSystemPasswordChar = true;
+            this.txtblankPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // picboxPASS
+            // 
+            this.picboxPASS.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.picboxPASS.Image = ((System.Drawing.Image)(resources.GetObject("picboxPASS.Image")));
+            this.picboxPASS.Location = new System.Drawing.Point(308, 216);
+            this.picboxPASS.Name = "picboxPASS";
+            this.picboxPASS.Size = new System.Drawing.Size(25, 20);
+            this.picboxPASS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picboxPASS.TabIndex = 5;
+            this.picboxPASS.TabStop = false;
+            this.picboxPASS.Click += new System.EventHandler(this.picboxPASS_Click);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLogin.BackColor = System.Drawing.Color.White;
+            this.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLogin.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.ForeColor = System.Drawing.Color.Black;
+            this.txtLogin.Location = new System.Drawing.Point(354, 104);
+            this.txtLogin.Multiline = true;
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtLogin.Size = new System.Drawing.Size(210, 26);
+            this.txtLogin.TabIndex = 4;
+            this.txtLogin.Text = "Login to your account";
+            this.txtLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnLogin.BackColor = System.Drawing.Color.Maroon;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(418, 261);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(98, 53);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.ForeColor = System.Drawing.Color.Gray;
+            this.txtUsername.Location = new System.Drawing.Point(339, 145);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(83, 15);
+            this.txtUsername.TabIndex = 9;
+            this.txtUsername.Text = "USERNAME";
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Gray;
+            this.txtPassword.Location = new System.Drawing.Point(339, 195);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(83, 15);
+            this.txtPassword.TabIndex = 10;
+            this.txtPassword.Text = "PASSWORD";
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtblankPassword);
+            this.Controls.Add(this.txtblankUsername);
+            this.Controls.Add(this.picboxPASS);
+            this.Controls.Add(this.picboxUSER);
+            this.Controls.Add(this.panelColor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panelColor.ResumeLayout(false);
+            this.panelColor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxUMLOGO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxUSER)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxPASS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelColor;
+        private System.Windows.Forms.PictureBox picboxUMLOGO;
+        private System.Windows.Forms.TextBox txtWelcome;
+        private System.Windows.Forms.TextBox txtDeveloperName;
+        private System.Windows.Forms.PictureBox picboxUSER;
+        private System.Windows.Forms.TextBox txtblankUsername;
+        private System.Windows.Forms.TextBox txtblankPassword;
+        private System.Windows.Forms.PictureBox picboxPASS;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ LoginForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_System_with_POS_for_UMVC_Canteen
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void picboxPASS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
