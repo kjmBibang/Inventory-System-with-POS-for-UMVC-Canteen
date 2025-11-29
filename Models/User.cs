8aad82c6fe60d1e18c060676b4de9d96f78281a8
Gabriel <r.mabanag.145839.tc@umindanao.edu.ph>
2025-11-29 16:31:18 +0800

    Updated Pascal Attributes to camel in User Class

--- Models/User.cs
+++ Models/User.cs
@@ -4,27 +4,27 @@ namespace Inventory_System_with_POS_for_UMVC_Canteen.Models
 {
     public abstract class User
     {
-        protected string UserID { get; }
-        protected string Username { get; }
-        protected string PasswordHash { get; }
-        protected int RoleID { get; }
+        protected string userID { get; }
+        protected string username { get; }
+        protected string passwordHash { get; }
+        protected int roleID { get; }
 
         protected User(string userID, string username, string passwordHash, int roleID)
         {
-            UserID = userID;
-            Username = username;
-            PasswordHash = passwordHash;
-            RoleID = roleID;
+            this.userID = userID;
+            this.username = username;
+            this.passwordHash = passwordHash;
+            this.roleID = roleID;
         }
 
         public string GetUsername()
         {
-            return Username;
+            return username;
         }
 
         public string GetUserID()
         {
-            return UserID;
+            return userID;
         }
     }
 }
