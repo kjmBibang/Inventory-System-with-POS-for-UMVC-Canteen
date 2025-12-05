9f2589a564e0aacf3fb232a883374f906ad5d2e7
Bryce San Jose <a.sanjose.145835.tc@umindanao.edu.ph>
2025-12-04 23:36:18 -0500

    Modified Interfaces

--- Interfaces/IUserManager.cs
+++ Interfaces/IUserManager.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Inventory_System_with_POS_for_UMVC_Canteen.Interfaces
 {
-    internal interface IUserManager
+    public interface IUserManager
     {
+        Models.User Login(string username, string password);
     }
 }
