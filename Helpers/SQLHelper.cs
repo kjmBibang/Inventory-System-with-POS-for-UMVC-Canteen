b24caddfd5fb0a8eb366b970359eb7956c259252
Kenn JB Mundiz <k.mundiz.146785.tc@umindanao.edu.ph>
2025-12-09 19:09:18 +0800

    Added SQL server helper

---
+++ Helpers/SQLHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_System_with_POS_for_UMVC_Canteen.Helpers
+{
+    internal class SQLHelper
+    {
+    }
+}
