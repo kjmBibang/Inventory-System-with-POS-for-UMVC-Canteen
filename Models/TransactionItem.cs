1f9c236cbfa9e3a04929c0912fc590273c39ca40
Kenn JB Mundiz <k.mundiz.146785.tc@umindanao.edu.ph>
2025-12-18 19:35:59 +0800

    Added Product and Transaction Classes in the Models Folder (#10)
    Co-authored-by: Gabriel <r.mabanag.145839.tc@umindanao.edu.ph>

---
+++ Models/TransactionItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_System_with_POS_for_UMVC_Canteen.Models
+{
+    public class TransactionItem
+    {
+        private int transactionItemID { get; set; }
+        private int transactionID { get; set; }
+        private int productID { get; set; }
+        private decimal unitPrice { get; set; }
+        private int quantity { get; set; }
+        private decimal subTotal { get; set; }
+
+        public TransactionItem(int transactionItemID, int transactionID, int productID, decimal unitPrice, int quantity, decimal subTotal)
+        {
+            this.transactionItemID = transactionItemID;
+            this.transactionID = transactionID;
+            this.productID = productID;
+            this.unitPrice = unitPrice;
+            this.quantity = quantity;
+            this.subTotal = subTotal;
+        }
+    }
+}
