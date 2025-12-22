4cddf3b46e0283f903f7947d3053735b3607dc14
Bryce San Jose <a.sanjose.145835.tc@umindanao.edu.ph>
2025-12-22 11:51:56 -0500

    Adding Dashboard and Small adjustment into LoginForm

---
+++ Dashboard.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_System_with_POS_for_UMVC_Canteen
+{
+    public partial class Dashboard : Form
+    {
+        private TextBox _activeTextBox;
+
+        private void TextBox_Enter(object sender, EventArgs e)
+        {
+            _activeTextBox = sender as TextBox;
+        }
+
+        public Dashboard()
+        {
+            InitializeComponent();
+
+            
+            txtBarcode.Enter += TextBox_Enter;
+            txtQuantity.Enter += TextBox_Enter;
+            txtSubtotal.Enter += TextBox_Enter;
+            txtTotal.Enter += TextBox_Enter;
+            txtCash.Enter += TextBox_Enter;
+            txtChange.Enter += TextBox_Enter;
+
+            btnNumber0.Click += NumberButton_Click;
+            btnNumber1.Click += NumberButton_Click;
+            btnNumber2.Click += NumberButton_Click;
+            btnNumber3.Click += NumberButton_Click;
+            btnNumber4.Click += NumberButton_Click;
+            btnNumber5.Click += NumberButton_Click;
+            btnNumber6.Click += NumberButton_Click;
+            btnNumber7.Click += NumberButton_Click;
+            btnNumber8.Click += NumberButton_Click;
+            btnNumber9.Click += NumberButton_Click;
+            btnDecimal.Click += NumberButton_Click;
+            btnClear.Click += btnClear_Click;
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            txtBarcode.Text = "0";
+            txtQuantity.Text = "0";
+            txtSubtotal.Text = "0";
+            txtTotal.Text = "0";
+            txtCash.Text = "0";
+            txtChange.Text = "0";
+        }
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            if (_activeTextBox == null) return;
+
+            Button btn = sender as Button;
+            if (btn == null) return;
+
+            string input = btn.Text;
+
+            if (input == ".")
+            {
+                if (!_activeTextBox.Text.Contains("."))
+                    _activeTextBox.Text += ".";
+            }
+            else
+            {
+                if (_activeTextBox.Text == "0")
+                    _activeTextBox.Text = input;
+                else
+                    _activeTextBox.Text += input;
+            }
+        }
+
+
+
+        private void btnNumber1_Click(object sender, EventArgs e)
+        {
+            
+               
+            
+        }
+
+        private void btnNumber2_Click(object sender, EventArgs e)
+        {
+            
+               
+        }
+
+        private void btnNumber3_Click(object sender, EventArgs e)
+        {
+            
+              
+            
+        }
+
+        private void btnNumber4_Click(object sender, EventArgs e)
+        {
+            
+               
+            
+        }
+
+        private void btnNumber5_Click(object sender, EventArgs e)
+        {
+            
+               
+        }
+
+        private void btnNumber6_Click(object sender, EventArgs e)
+        {
+            
+              
+            
+        }
+
+        private void btnNumber7_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void btnNumber8_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void btnNumber9_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void btnNumber0_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void txtBarcode_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtSubtotal_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtCash_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtChange_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+        
+            if (_activeTextBox == null) return;
+
+            _activeTextBox.Text = "0";
+        
+
+    }
+}
+}
