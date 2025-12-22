f76b850905ba58aed8c20ee66aa186644b02d9c6
Gabriel <r.mabanag.145839.tc@umindanao.edu.ph>
2025-12-22 19:25:46 +0800

    Added MockDB, SQLTransactionRepo, ITransactionRepo, and TransactionManager

---
+++ Interfaces/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Inventory_System_with_POS_for_UMVC_Canteen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_System_with_POS_for_UMVC_Canteen.Interfaces
+{
+    public interface ITransactionRepository
+    {
+         Transaction GetTransaction();
+         Transaction StoreTransaction(Transaction transaction);
+    }
+}
