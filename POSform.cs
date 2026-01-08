3b72048e74bcd7c3879377b42d91b3beda7b715b
Kenn JB Mundiz <k.mundiz.146785.tc@umindanao.edu.ph>
2026-01-08 18:38:06 +0800

    deleted a duplicate method

--- POSform.cs
+++ POSform.cs
@@ -847,49 +847,6 @@ namespace Inventory_System_with_POS_for_UMVC_Canteen
         }
         
 
-        private void LoadProductByBarcode(string barcode)
-        {
-            using (SqlConnection con = new SqlConnection(serverHelper.GetConnectionString()))
-            {
-                string query = @"
-        SELECT Barcode, ProductName, Price, Stock
-        FROM Products
-        WHERE Barcode = @Barcode";
-
-                using (SqlCommand cmd = new SqlCommand(query, con))
-                {
-                    cmd.Parameters.AddWithValue("@Barcode", barcode);
-                    con.Open();
-
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            int stock = Convert.ToInt32(reader["Stock"]);
-
-                            if (stock <= 0)
-                            {
-                                MessageBox.Show("Out of stock");
-                                return;
-                            }
-
-                            // add to grid
-                            AddProductToGrid(
-                                reader["Barcode"].ToString(),
-                                reader["ProductName"].ToString(),
-                                Convert.ToDecimal(reader["Price"])
-                            );
-
-                            // reduce stock
-                            ReduceStock(barcode);
-                        }
-                        else
-                        {
-                            MessageBox.Show("Product not found");
-                        }
-                    }
-                }
-            }
-        }
+        
     }
 }
