3dfbfd1ab0a2e01c4942ea4f23bdf59e91824467
Bryce San Jose <a.sanjose.145835.tc@umindanao.edu.ph>
2026-01-09 10:01:46 -0500

    Improving Stock Adjustment Form

--- StockAdjustmentForm.Designer.cs
+++ StockAdjustmentForm.Designer.cs
@@ -31,101 +31,144 @@ namespace Inventory_System_with_POS_for_UMVC_Canteen
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblProduct = new System.Windows.Forms.Label();
             this.lblProductPlaceholder = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtQuantity = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.gradient_Panel1 = new Inventory_System_with_POS_for_UMVC_Canteen.UIHelpers.Gradient_Panel();
+            this.gradient_Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(117, 23);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(49, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(307, 32);
+            this.label1.Size = new System.Drawing.Size(331, 32);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Stock Adjustment Form";
+            this.label1.Text = "STOCK ADJUSTMENT FORM";
             // 
-            // label2
+            // lblProduct
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(82, 126);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Product:";
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.BackColor = System.Drawing.Color.Transparent;
+            this.lblProduct.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblProduct.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProduct.ForeColor = System.Drawing.Color.White;
+            this.lblProduct.Location = new System.Drawing.Point(60, 76);
+            this.lblProduct.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(106, 25);
+            this.lblProduct.TabIndex = 1;
+            this.lblProduct.Text = "PRODUCT:";
             // 
             // lblProductPlaceholder
             // 
             this.lblProductPlaceholder.AutoSize = true;
-            this.lblProductPlaceholder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProductPlaceholder.Location = new System.Drawing.Point(173, 126);
+            this.lblProductPlaceholder.BackColor = System.Drawing.Color.Transparent;
+            this.lblProductPlaceholder.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblProductPlaceholder.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductPlaceholder.ForeColor = System.Drawing.Color.White;
+            this.lblProductPlaceholder.Location = new System.Drawing.Point(170, 76);
+            this.lblProductPlaceholder.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblProductPlaceholder.Name = "lblProductPlaceholder";
-            this.lblProductPlaceholder.Size = new System.Drawing.Size(85, 25);
+            this.lblProductPlaceholder.Size = new System.Drawing.Size(106, 25);
             this.lblProductPlaceholder.TabIndex = 2;
-            this.lblProductPlaceholder.Text = "Product:";
+            this.lblProductPlaceholder.Text = "PRODUCT:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(82, 183);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(60, 124);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.Size = new System.Drawing.Size(76, 25);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Stock:";
+            this.label3.Text = "STOCK:";
             // 
             // txtQuantity
             // 
             this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtQuantity.Location = new System.Drawing.Point(178, 180);
+            this.txtQuantity.Location = new System.Drawing.Point(149, 125);
+            this.txtQuantity.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtQuantity.Name = "txtQuantity";
-            this.txtQuantity.Size = new System.Drawing.Size(235, 30);
+            this.txtQuantity.Size = new System.Drawing.Size(214, 26);
             this.txtQuantity.TabIndex = 4;
             this.txtQuantity.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnSave
             // 
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(162, 229);
+            this.btnSave.BackColor = System.Drawing.Color.Transparent;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(135, 172);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(109, 52);
+            this.btnSave.Size = new System.Drawing.Size(82, 42);
             this.btnSave.TabIndex = 5;
             this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(450, 247);
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(233, 172);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(82, 42);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // gradient_Panel1
+            // 
+            this.gradient_Panel1.ColorBottom = System.Drawing.Color.Maroon;
+            this.gradient_Panel1.ColorTop = System.Drawing.Color.DarkOrange;
+            this.gradient_Panel1.Controls.Add(this.btnSave);
+            this.gradient_Panel1.Controls.Add(this.btnCancel);
+            this.gradient_Panel1.Controls.Add(this.label1);
+            this.gradient_Panel1.Controls.Add(this.lblProduct);
+            this.gradient_Panel1.Controls.Add(this.txtQuantity);
+            this.gradient_Panel1.Controls.Add(this.lblProductPlaceholder);
+            this.gradient_Panel1.Controls.Add(this.label3);
+            this.gradient_Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gradient_Panel1.Location = new System.Drawing.Point(0, 0);
+            this.gradient_Panel1.Name = "gradient_Panel1";
+            this.gradient_Panel1.Size = new System.Drawing.Size(416, 238);
+            this.gradient_Panel1.TabIndex = 7;
             // 
             // StockAdjustmentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 293);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.txtQuantity);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.lblProductPlaceholder);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(416, 238);
+            this.Controls.Add(this.gradient_Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StockAdjustmentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Stock Adjustment Form";
             this.Load += new System.EventHandler(this.StockAdjustmentForm_Load);
+            this.gradient_Panel1.ResumeLayout(false);
+            this.gradient_Panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -147,11 +190,12 @@ namespace Inventory_System_with_POS_for_UMVC_Canteen
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblProduct;
         private System.Windows.Forms.Label lblProductPlaceholder;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtQuantity;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
+        private UIHelpers.Gradient_Panel gradient_Panel1;
     }
 }
